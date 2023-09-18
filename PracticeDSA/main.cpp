2be434992179663f7c5573040606f875117495d5
Gitesh Goyal <goyalg1017@gmail.com>
2023-09-18 10:41:40 +0530

    Implementation of Height of Binary Tree

--- PracticeDSA/main.cpp
+++ PracticeDSA/main.cpp
@@ -5,10 +5,37 @@
 //  Created by Goyal, Gitesh on 27/05/23.
 //
 
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int key;
+    struct Node *left;
+    struct Node *right;
+    
+    Node(int k) {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+int height(Node* root) {
+    if(root == NULL) return 0;
+    
+    return max(height(root->left),height(root->right)) + 1;
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Node *root=new Node(10);
+    root->left=new Node(20);
+    root->right=new Node(30);
+    root->right->left=new Node(40);
+    root->right->right=new Node(50);
+    
+    cout<<"Height of Tree -> "<<height(root)<<endl;
+    
     return 0;
 }
+
